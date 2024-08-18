9bccc5b0ae7179ef994f3a1814b1aa9ac84fdc0f
MateuszSzeliga <80278770+SzeligaMateusz@users.noreply.github.com>
2024-08-18 21:17:09 +0200

    Update bin.py

--- bin.py
+++ bin.py
@@ -15,7 +15,7 @@
 # 1. Take your PNG file to folder and rename to "input.png".                                      #
 # 2. Compile the program                    (!)_You must have a Pillow ("pip install pillow")_(!) #
 # 3. Now you can see the output file (output_year_month_day-hour_minute)                          #
-# 4. Copy the contents of the file and Ctrl-V to your arduino project.                            #
+# 4. Copy the contents of the file and Ctrl-V to your project.                            #
 #-------------------------------------------------------------------------------------------------#
 #-------------------------------# Thanks, good luck with your code :P #-------------------------- #
 #-------------------------------------------------------------------------------------------------#
